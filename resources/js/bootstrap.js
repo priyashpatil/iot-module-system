4364c5024968b506f6f2cfef5e27e8bf3aa6a57d
Priyash <38959321+priyashpatil@users.noreply.github.com>
2024-11-07 19:16:36 +0530

    feat: add bootstrap css framework

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,5 @@
+import { Dropdown, Modal } from 'bootstrap';
 import axios from 'axios';
-window.axios = axios;
 
+window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
